d4abd63c048fc29b84574e3488f576cba9a1fc8e
agyesam12 <agyesam12@gmail.com>
2025-10-24 11:36:34 +0000

    updated the Report.cs files

--- ArtisanMarketPlace/Models/Report.cs
+++ ArtisanMarketPlace/Models/Report.cs
@@ -3,6 +3,7 @@ using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Collections.Generic;
 
+
 namespace ArtisanMarketPlace.Models
 {
     // Corresponds to the Django Report model
